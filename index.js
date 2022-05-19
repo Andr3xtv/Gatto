3d5951d590b80ffb1cfaac925de8b0f9ce422679
Andr3xtv <99839032+Andr3xtv@users.noreply.github.com>
2022-05-19 15:48:03 +0200

    Upload files

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js")
+const client = new Discord.Client(
+    {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]}
+    )
+    
+client.login("OTc2NTcwNzA4MDk0MjM0NjI0.GMPVrO.zLMyAFA133rsBQUtrMmB7ufSH3iequvhD42XsY")
+
+client.on("ready", () => {
+    console.log("BOT ONLINE")
+})
+
+client.on("messageCreate", (message) =>{
+    if(message.content == "!skribbl"){
+        message.channel.send("https://skribbl.io")
+    }
+
+    if(message.content == "ciao") {
+        message.channel.send("ciao a te pirla")
+    }  
+
+    if(message.content == "!osu") {
+        message.channel.send("https://osu.ppy.sh/home")
+    } 
+    
+    if(message.content == "!valorant") {
+        message.channel.send("https://playvalorant.com/it-it/agents/")
+    } 
+
+    if(message.content == "!spotify") {
+        message.channel.send("Ecco la migliore playlist del mondo: https://open.spotify.com/playlist/6aFrZfPb2Rd7E6XRQEqZ9U?si=58dfb3c7ca094a16")
+    } 
+
+})
+
+
+
+
